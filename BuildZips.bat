6837f9effc8d92673d671bbe24f65329d7e2599e
ChatFawkes <chatfawkes@gmail.com>
2015-02-27 23:50:43 -0500

    Revamp downloading of fastcraft - Now handled completely by gradle - Will only download if new version isn't detected - Will delete old versions if present in the output directory
    Signed-off-by: ChatFawkes <chatfawkes@gmail.com>

--- BuildZips.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-IF EXIST output/fastcraft*.jar (
-CALL gradlew.bat
-) else (
-CALL gradlew.bat -Pfastcraft=true
-)
-pause
--- BuildZips.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -f output/fastcraft*.jar ]; then
-    bash gradlew
-else
-    bash gradlew -Pfastcraft=true
-fi
